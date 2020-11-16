f6efe6d47924e6b766f5ebca2080be2b48914d3b
Giovana Furlan <giovananelofurlan@gmail.com>
2020-11-15 22:51:28 -0300

    :up: JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.querySelector ('.abacaxi').addEventListener ('click', () => {
     document.querySelector ('.container').classList.toggle ('show-menu');
 });
+
+function Enviar() {
+
+    var nome = document.getElementById("nome");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+}
